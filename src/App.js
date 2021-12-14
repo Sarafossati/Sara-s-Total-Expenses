feb40a3078905a52f4dc81e2900fa2278c32839f
Sara Fossati <sarafossati6@gmail.com>
2021-12-14 08:55:46 +0100

    develop

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+
+import Expenses from "./components/Expenses/Expenses.js";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "iPhone 13",
+      amount: 999.99,
+      date: new Date(2021, 7, 14),
+    },
+    { 
+      id: "e2", 
+      title: "Holidays", 
+      amount: 345.49, 
+      date: new Date(2020, 5, 12) },
+    {
+      id: "e3",
+      title: "New Laptop",
+      amount: 699.95,
+      date: new Date(2021, 12, 28),
+    },
+    {
+      id: "e4",
+      title: "Apple Watch",
+      amount: 499.99,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-
-
-        
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Sara's Total Expenses</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
